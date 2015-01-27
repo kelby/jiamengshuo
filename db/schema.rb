8dc57002848f393a45b4ede94f135406b6bb4cfc
kelby <kelby@3cps.ca>
2015-01-27 21:55:53 +0800

    add question scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126142214) do
+ActiveRecord::Schema.define(version: 20150127135036) do
 
   create_table "admin_catalogs", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "body",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "taggings", force: :cascade do |t|
