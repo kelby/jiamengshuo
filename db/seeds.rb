5c5e5a769703e33c4593086b15c0d6d187581c90
kelby <kelby@3cps.ca>
2015-01-31 14:17:18 +0800

    seed data for catalog

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,39 @@ end
 
 Wish.create!(content: "我老爸不断提醒我：“干活一定要找个好师傅，跟他学习，对你很有帮助，一定要找一个师傅。”<br>
               我希望在这个网站里，能找到自己的导师；或者发现欣赏的人，成为他的导师。", user_id: User.first.id)
+
+=begin
+['个人发展', '职场', '兴趣爱好', '生活', '设计师', '语言', 'IT技术', '投资理财', '演讲', '其它'].each do |name|
+  Catalog.create!(name: name)
+end
+
+['创业', '管理', '沟通技巧'].each do |name|
+  catalog = Catalog.create!(name: name)
+  catalog.parent = Catalog.find_by(name: "职场")
+  catalog.save
+end
+
+['写小说', '拍微电影', '开咖啡馆'].each do |name|
+  Catalog.create!(name: name, parent: Catalog.find_by(name: "兴趣爱好"))
+end
+
+['购物', '美食', '理财', '健康', '运动'].each do |name|
+  Catalog.create!(name: name, parent: Catalog.find_by(name: "生活"))
+end
+
+['艺术', '创意'].each do |name|
+  Catalog.create!(name: name, parent: Catalog.find_by(name: "设计师"))
+end
+
+['英语', '课程', '书箱'].each do |name|
+  Catalog.create!(name: name, parent: Catalog.find_by(name: "语言"))
+end
+
+['操作系统', 'ios', 'android', 'web开发'].each do |name|
+  Catalog.create!(name: name, parent: Catalog.find_by(name: "IT技术"))
+end
+
+['产品', '需求分析', 'axure'].each do |name|
+  Catalog.create!(name: name, parent: Catalog.find_by(name: "IT技术"))
+end
+=end
