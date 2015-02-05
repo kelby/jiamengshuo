1a40a1285357783d570262843c580c76ba0aa32e
kelby <kelby@3cps.ca>
2015-02-06 00:05:01 +0800

    subject index, and subject association desc_long_comment.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,4 +3,6 @@ class Subject < ActiveRecord::Base
   has_many :posts, through: :sections
   has_many :comments, as: :commentable, dependent: :destroy
   belongs_to :user
+
+  has_one :desc_long_comment, -> { where("LENGTH(content) > ?", 140).order("id desc").limit(1) }, as: :commentable, class_name: 'Comment', foreign_key: :commentable_id
 end
