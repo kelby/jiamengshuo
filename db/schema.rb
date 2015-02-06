8aaf27054b7f33743a98fd56db83b7744c012bd8
kelby <kelby@3cps.ca>
2015-02-06 22:15:52 +0800

    posts index, show; topics index, show; change description.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206054552) do
+ActiveRecord::Schema.define(version: 20150206135444) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -97,10 +97,10 @@ ActiveRecord::Schema.define(version: 20150206054552) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",       limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.integer  "user_id",     limit: 4
-    t.string   "description", limit: 255
+    t.text     "description", limit: 65535
     t.string   "icon",        limit: 255
   end
 
