1742768b7fc8c7b56609a9c6ff6e375421037c11
kelby <kelby@3cps.ca>
2015-02-16 00:04:03 +0800

    use new contact link and controller, model.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -7,6 +7,6 @@ class ContactController < ApplicationController
     contact = Contact.new params[:contact]
     Contact.delay.deliver(contact) if contact.valid?
 
-    redirect_to root_path
+    redirect_to root_path, notice: "邮件发送成功，感谢您的反馈。"
   end
 end
