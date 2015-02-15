182484a825210dcd320c1b74579e114762c36280
kelby <kelby@3cps.ca>
2015-02-15 13:57:46 +0800

    config active_job queue_adapter is sidekiq

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module FoundMentor
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
 
