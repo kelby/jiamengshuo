b550d4d427cf972ce6cd6dc5f9931d3a993f6cf8
kelby <kelby@3cps.ca>
2015-03-03 01:26:48 +0800

    only show specile public activity.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,13 @@ class PagesController < ApplicationController
 
   def index
     @topic = Topic.essences.order("updated_at DESC").last
-    @activities = PublicActivity::Activity.order("created_at desc").page(params[:pub_page] || 1).per(15)
+    condition = {key: ["user.follow", "user.stop_following", "user.create",
+      "apply.approve_apply", "apply.refuse_apply", "apply.create",
+      "reply.create",
+      "keeper_topic.keep", "marker_topic.mark", "keeper_topic.unkeep", "marker_topic.unmark",
+      "wish.spurn_it", "wish.checkout_it", "wish.checkin_it", "wish.got_it"
+      ]}
+    @activities = PublicActivity::Activity.order("created_at desc").page(params[:pub_page] || 1).per(15).where.not(condition)
     @snippet = @topic.snippet
   end
 end
