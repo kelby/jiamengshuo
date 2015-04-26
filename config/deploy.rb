f5fc35c3e9a57f4f0385a44be7e535062c75149f
Kelby Lee <leekelby@gmail.com>
2015-04-26 17:35:56 +0800

    do not link bin dir.

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,7 +63,7 @@ set :pty, true
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 set :linked_files, %w{config/database.yml config/mailer.yml config/redis.yml config/secrets.yml config/sidekiq.yml config/initializers/devise.rb}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/images public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/images public/uploads}
 
 namespace :deploy do
 
