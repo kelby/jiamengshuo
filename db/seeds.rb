b4018aff5536072cef1eec3cc4684e2f926d9c33
Joy Neop <joyneop@gmail.com>
2015-05-22 09:16:55 +0000

    changed index page listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,7 +96,7 @@ end
   Section.create!(user_id: User.ids.sample, heading: Faker::Name.name, body: Faker::Lorem.paragraph(2))
 end
 
-# user = User.all.sample # User.create!(username: 'Caleb Winters', password: 'password', email: "Caleb_Winters@gmail.com", faker: true)
+user = User.all.sample # User.create!(username: 'Caleb Winters', password: 'password', email: "Caleb_Winters@gmail.com", faker: true)
 100.times do
   topic = Topic.create!(title: Faker::Name.title, user: User.all.sample, body: Faker::Lorem.paragraph,
                         category: Topic.categories.keys.sample,
@@ -109,6 +109,7 @@ end
 
 # Topic.order('id desc').limit(100).destroy_all
 
+topic = Topic.all.sample
 comment = Comment.create!(user: user, content: "
           <p>Nice start, Becky. I really like some of the elements you have going so far. The header and footer tie in nicely and the ribbon on tiles works well. The font is great also and looks readable. Here are a few areas you can keep iterating on:</p>
 
