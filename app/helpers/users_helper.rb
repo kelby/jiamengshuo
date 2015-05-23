b049d0e4a7ca78398886e5a273af42244892d8a9
Kelby Lee <leekelby@gmail.com>
2015-05-23 19:41:28 +0800

    代码整理；添加忘记密码的页面

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,13 @@ module UsersHelper
   def cn_for_user_info(info)
     info.present? ? info : "这家伙很懒,什么也没留下~"
   end
+
+  def cn_for_provider(provider)
+    case provider
+    when :weibo
+      "微博"
+    when :qq_connect
+      "QQ"
+    end
+  end
 end
