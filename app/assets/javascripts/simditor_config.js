2799d4f1a757eaf579f29a39e28a310a3b769009
free1 <747549945@qq.com>
2015-05-25 16:22:23 +0800

    编辑器功能调整

--- app/assets/javascripts/simditor_config.js
+++ app/assets/javascripts/simditor_config.js
@@ -1,3 +1,4 @@
 var editor = new Simditor({
-  textarea: $('#editor')
+  textarea: $('#editor'),
+	toolbar: ['title', 'bold', 'italic', 'underline', 'strikethrough', 'color', 'ol', 'ul', 'blockquote', 'table', 'link', 'image', 'hr']
 });
