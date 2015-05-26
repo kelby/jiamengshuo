5117aa6953fb72c2d82a0fe7663d232a0180d363
ibcker <happymiyu@gmail.com>
2015-05-26 21:08:31 +0800

    去掉用户qq，电话验证（不知道允许为nil）

--- app/models/user_body.rb
+++ app/models/user_body.rb
@@ -1,7 +1,7 @@
 class UserBody < ActiveRecord::Base
   belongs_to :user
-  validates_format_of :qq, :with => /\A\d{5,15}\z/
-  validates_format_of :phone, :with => /\A[0-9\-]{7,14}\z/
+  # validates_format_of :qq, :with => /\A\d{5,15}\z/
+  # validates_format_of :phone, :with => /\A[0-9\-]{7,14}\z/
 
   enum gender: { default: 0, male: 1, female: 2 }
   
