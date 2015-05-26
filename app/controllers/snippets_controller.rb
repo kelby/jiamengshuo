9526dd3e8d11a969201b820cee885f84b016412c
Kelby Lee <leekelby@gmail.com>
2015-05-26 22:32:15 +0800

    拼单拒绝或同意后得到消息

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -15,7 +15,10 @@ class SnippetsController < ApplicationController
     @topic = Topic.find params[:topic_id]
 
     @snippet = @topic.snippets.build(snippet_params.merge(user_id: current_user.id))
-    @snippet.save
+    if @snippet.save
+      @direct_message = DirectMessage.new(from_user_id: current_user.id, content: "#{current_user.username} 申请了你的发单", to_user_id: @topic.user_id)
+      @direct_message.save
+    end
 
     redirect_to @topic, notice: "跟单请求提交成功，等待盟主的批准"
   end
