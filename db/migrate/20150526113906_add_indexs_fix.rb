b7fd83bd11c4b74036f4bcccf1fd2cb4ab65d0d7
ibcker <happymiyu@gmail.com>
2015-05-26 19:44:47 +0800

    add_index_fix

---
+++ db/migrate/20150526113906_add_indexs_fix.rb
@@ -0,0 +1,54 @@
+class AddIndexsFix < ActiveRecord::Migration
+  def change
+    remove_index :topics, [:title,:user_id,:catalog_id]
+    remove_index :comments, [:user_id, :commentable_id]
+    remove_index :replies, [:user_id, :comment_id]
+    remove_index :user_bodies, [:user_id]
+    remove_index :user_relationships, [:owner_id,:recipient_id]
+    remove_index :user_wishes, [:wish_id,:user_id]
+    remove_index :wishes, [:user_id]
+    remove_index :direct_messages, [:to_user_id,:from_user_id]
+    remove_index :sections, [:post_id,:user_id,:subject_id]
+    remove_index :liker_comments, [:liker_id]
+    remove_index :snippets, [:topic_id,:user_id]
+    remove_index :topic_and_users, [:topic_id,:user_id]
+
+
+    #fix
+    add_index :topics, :title
+    add_index :topics, :user_id
+    add_index :topics, :catalog_id
+
+    add_index :comments, :user_id
+    add_index :comments, :commentable_id
+
+    add_index :replies, :user_id
+    add_index :replies, :comment_id
+
+    add_index :user_bodies, :user_id
+
+    add_index :user_relationships, :owner_id
+    add_index :user_relationships, :recipient_id
+
+    add_index :user_wishes, :wish_id
+    add_index :user_wishes, :user_id
+
+    add_index :wishes, :user_id
+
+    add_index :direct_messages, :to_user_id
+    add_index :direct_messages, :from_user_id
+
+    add_index :sections, :post_id
+    add_index :sections, :user_id
+    add_index :sections, :subject_id
+
+    add_index :liker_comments, :liker_id
+
+    add_index :snippets, :topic_id
+    add_index :snippets, :user_id
+
+    add_index :topic_and_users, :topic_id
+    add_index :topic_and_users, :user_id
+
+  end
+end
