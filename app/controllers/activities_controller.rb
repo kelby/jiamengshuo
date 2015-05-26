a9f894c446c303c96e71f2adffebd2304fb63521
Kelby Lee <leekelby@gmail.com>
2015-05-26 14:17:43 +0800

    用户 show 和 profile页面的feed.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    @to_my_resource = PublicActivity::Activity.where(key: ["keeper_topic.keep", "marker_topic.mark", "reply.create", "comment.create", "liker_comment.like_it"]).where.not(owner_id: current_user.id).page(params[:page])
+    @to_my_resource = PublicActivity::Activity.where(key: ["keeper_topic.keep", "reply.create", "comment.create", "liker_comment.like_it"]).where.not(owner_id: current_user.id).page(params[:page])
     @to_my_request = PublicActivity::Activity.where(key: ["snippet.approve", "snippet.refuse"]).where.not(owner_id: current_user.id).page(params[:page])
     @follower_to_me = PublicActivity::Activity.where(key: "user.follow").where(recipient_id: current_user.id).page(params[:page])
 
