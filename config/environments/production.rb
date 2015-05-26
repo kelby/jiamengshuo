036689d84d4bf2a889cbfd2ca39327546cadf03f
ibcker <happymiyu@gmail.com>
2015-05-26 19:35:24 +0800

    Revert "修改发邮件端口号"
    This reverts commit a748fbe9dcb071925f76ec7cb268fd6cc9175493.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.default_url_options = { host: 'jiamengshuo.com' , port: 3000}
+  config.action_mailer.default_url_options = { host: 'jiamengshuo.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
