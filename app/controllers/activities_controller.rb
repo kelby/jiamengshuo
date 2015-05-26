8bf4ad32bdafff2e39963a07b84c4dc86fb34e94
Kelby Lee <leekelby@gmail.com>
2015-05-26 16:13:14 +0800

    Ajax更新消息列表里的内容

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,11 +2,16 @@ class ActivitiesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    @to_my_resource = PublicActivity::Activity.where(key: ["keeper_topic.keep", "reply.create", "comment.create", "liker_comment.like_it"]).where.not(owner_id: current_user.id).page(params[:page])
-    @to_my_request = PublicActivity::Activity.where(key: ["snippet.approve", "snippet.refuse"]).where.not(owner_id: current_user.id).page(params[:page])
-    @follower_to_me = PublicActivity::Activity.where(key: "user.follow").where(recipient_id: current_user.id).page(params[:page])
+    @to_my_resource = PublicActivity::Activity.where(key: ["keeper_topic.keep", "reply.create", "comment.create", "liker_comment.like_it"]).where.not(owner_id: current_user.id).page(params[:to_my_resource]).per(15)
+    @to_my_request = PublicActivity::Activity.where(key: ["snippet.approve", "snippet.refuse"]).where.not(owner_id: current_user.id).page(params[:to_my_request]).per(15)
+    @follower_to_me = PublicActivity::Activity.where(key: "user.follow").where(recipient_id: current_user.id).page(params[:follower_to_me]).per(15)
 
-    @direct_messages = DirectMessage.where(to_user_id: current_user.id).page(params[:dm_page] || 1).per(15)
+    @direct_messages = DirectMessage.where(to_user_id: current_user.id).page(params[:direct_messages]).per(15)
     current_user.read_direct_messages(@direct_messages)
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
